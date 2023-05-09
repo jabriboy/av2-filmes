ec7aee2fefe5639bffe4bb3a4bac0d6a7de6e754
Gabriel Rocha <jabrirocha@hotmail.com>
2023-05-08 21:08:36 -0300

    first commit

---
+++ app/src/main/java/com/senaicimatec/av2_filmes/MainActivity.java
@@ -0,0 +1,14 @@
+package com.senaicimatec.av2_filmes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
